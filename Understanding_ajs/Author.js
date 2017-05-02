9038f18e435f59567a85c56f2545bde9b14e5540
rahnis <rahulnishant13@gmail.com>
2017-05-02 08:20:11 +0530

    directive and services example

---
+++ Understanding_ajs/Author.js
@@ -0,0 +1,11 @@
+﻿/// <reference path="angular.js" />
+
+var AuthorApp = angular.module('AuthorApp', []);
+AuthorApp.controller('MyController', function ($scope) {
+    $scope.authors = [
+        { 'name': 'Maruti Makwana' },
+        { 'name': 'Rahul Nishnat' },
+        { 'name': 'Ram Sorani' },
+        { 'name': 'Panda'}
+    ];
+});
